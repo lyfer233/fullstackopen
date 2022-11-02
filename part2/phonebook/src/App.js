854b76e62c7505f015c3c4e1c159faffdc688245
lyfer233 <liyf994@nenu.edu.cn>
2022-11-02 11:38:11 +0800

    finished problem 2.19

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,12 +3,14 @@ import Filter  from './component/Filter'
 import PersonForm from './component/PersonForm'
 import Persons from './component/Persons'
 import connectService from './services/persons'
+import Notification from './component/Notification'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNumber] = useState('')
   const [findName, setFindName] = useState('')
+  const [tipMessage, setTipMessage] = useState(null)
 
   // Get the data from the server.
   useEffect(() => {
@@ -42,6 +44,7 @@ const App = () => {
           setPersons(persons.concat(addedPerson))
           setNewName('')
           setNumber('')
+          setTipMessage(`Add ${addedPerson.name} successful~`)
       })
     } else {
       // Check whethe person information should update.
@@ -51,6 +54,7 @@ const App = () => {
           .replaceData(replacePerson.id, newPersonObject)
           .then(updatedPerson => {
             setPersons(persons.map(person => person.id === replacePerson.id ? updatedPerson : person))
+            setTipMessage(`Update ${updatedPerson.name} successful~`)
           })
           .catch(error => {
             alert('Warning! Update failed, Please check console')
@@ -78,6 +82,8 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={tipMessage}/>
 
       <Filter filterValue={findName} filterFunction={changeFindName} />
 
